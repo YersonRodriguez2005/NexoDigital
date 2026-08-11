6bc29ca529dd623bdec7b44d4b49df83f11b0dd1
Yerson Fabian Garzon Rodriguez <141519552+YersonRodriguez2005@users.noreply.github.com>
2026-08-11 17:33:28 -0500

    Update Price

--- src/data/DataProducts.js
+++ src/data/DataProducts.js
@@ -6,9 +6,9 @@ export const softwareProducts = [
     title: "InvenSync",
     description: "Sistema inteligente de Punto de Venta (POS) y control de inventario en tiempo real.",
     fullDescription: "InvenSync es una plataforma integral diseñada para modernizar y agilizar comercios locales. Ofrece un sistema de caja rápida (POS), sincronización de inventario al instante, reportes de ventas y una aplicación móvil nativa que permite escanear productos directamente desde el celular. Olvídate del desorden y toma el control total de las finanzas de tu negocio.",
-    price: "50.000",       
-    monthlyFee: "10.000",
-    regularPrice: "100.000",
+    price: "0",       
+    monthlyFee: "15.000",
+    regularPrice: "50.000",
     imageUrl: "/assets/InvenSync_images/image-main.png",
     gallery: [
       "/assets/InvenSync_images/dashboard.png",
